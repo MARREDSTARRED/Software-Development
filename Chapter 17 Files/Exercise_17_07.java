7e68b50f1de090f8995d692a1865a4402a750f3d
MARREDSTARRED <marredstarred@gmail.com>
2025-05-27 08:18:30 -0600

    Chapter 17 project files

---
+++ Chapter 17 Files/Exercise_17_07.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Exercise_17_07 {
+	public static void main(String[] args) 
+			throws IOException, ClassNotFoundException {
+		try ( 
+			ObjectInputStream input = new ObjectInputStream(new 
+				BufferedInputStream(new FileInputStream("Exercise17_07.dat")))
+		) { 
+			while (true) {
+				Loan loan = (Loan)input.readObject();
+				System.out.println(loan);
+				System.out.printf("Total loan amount: $%.2f\n", 
+					loan.getTotalPayment());
+				System.out.println();
+			}
+		}
+		catch (EOFException ex) {
+			
+		}
+	}
+}
